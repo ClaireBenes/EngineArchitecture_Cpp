19668814f01128d00ce3056c1c1bb02b79b12a6b
Claire Benes <claire.benes@artfx.fr>
2024-11-12 15:23:41 +0100

    [wip] end of class

--- EngineArchitecture/EngineArchitecture.cpp
+++ EngineArchitecture/EngineArchitecture.cpp
@@ -3,12 +3,25 @@
 #include <SDL.h>
 #include <glew.h>
 
+#include "Vector2.h"
+#include "Log.h"
+#include "Game.h"
+#include "Pong.h"
+
 using namespace std;
 
 #define GLEW_STATIC
 
 int main(int argc, char* argv[])
 {
+	Vector2 newVector = Vector2::ONE;
+	newVector *= 2.0f;
+	Log::Info(newVector.ToString());
+	std::cin.get();
+
+	Game* pong = new Game("Claire's Game Engine", { new Pong });
+	pong->Initialize();
+
 	return 0;
 }
 

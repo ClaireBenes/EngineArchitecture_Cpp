4cc39f12e44ecc9663d81d2ba90d614f77ec7179
Claire Benes <claire.benes@artfx.fr>
2024-11-12 15:22:15 +0100

    [add] class window

---
+++ EngineArchitecture/Window.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "SDL.h"
+
+#include "Log.h"
+#include "Vector2.h"
+
+class Window
+{
+public:
+	Window(int pWidth, int pHeight);
+	Window(const Window&) = delete;
+
+	Window& operator=(const Window&) = delete;
+	Vector2 GetDimensions() const;
+	SDL_Window* GetSdlWindow() const;
+
+	bool Open();
+	void Update();
+	void Close();
+
+private:
+	Vector2 mDimensions = {0,0};
+	SDL_Window* mSdlWindow;
+};
+

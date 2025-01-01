87794eb95d22110fbd17d393f4b819bb99c3e9cb
Bilou4000 <153065643+Bilou4000@users.noreply.github.com>
2025-01-01 16:35:12 +0100

    [wip] new space invaders scene

--- EngineArchitecture/Source/Game/EngineArchitecture.cpp
+++ EngineArchitecture/Source/Game/EngineArchitecture.cpp
@@ -6,10 +6,12 @@
 #include "Engine/Engine.h"
 
 #include "Pong.h"
+#include "SpaceInvaders.h"
 
 int main(int argc, char* argv[])
 {
-	Engine pong ("Claire's Game Engine", { new Pong });
+	//Engine pong ("Claire's Game Engine", { new Pong });
+	Engine spaceInvaders("Claire wants to destroy smt", { new SpaceInvaders });
 
 	return 0;
 }

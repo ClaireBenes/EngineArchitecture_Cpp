4fbbe89eabb9739b4a31b10c653bfcf1083fa382
Claire Benes <claire.benes@artfx.fr>
2025-03-03 15:12:36 +0100

    [add] shader class

---
+++ EngineArchitecture/Source/Engine/Shader/Shader.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+enum ShaderType
+{
+	VERTEX,
+	FRAGMENT,
+};
+
+class Shader
+{
+public:
+	Shader();
+	Shader(int pId, std::string pFile, ShaderType pShaderType);
+	~Shader() {};
+
+	int GetID() const { return mId; }
+	void Load(std::string pFileName, ShaderType pShaderType);
+
+	//Feel free to change the shader path  if needed
+	static const std::string SHADER_PATH;
+
+	//Contents of the text file
+	std::string& GetCode();
+
+protected:
+	unsigned int mId;
+	std::string mCode;
+	ShaderType mType;
+};
+

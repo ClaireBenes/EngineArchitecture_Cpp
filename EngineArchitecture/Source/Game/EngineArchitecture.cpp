8dd2a32a73257f6d33298c7def6b0b6f7a763860
Claire Benes <claire.benes@artfx.fr>
2025-04-04 17:39:13 +0200

    [add] gyroid asset

--- EngineArchitecture/Source/Game/EngineArchitecture.cpp
+++ EngineArchitecture/Source/Game/EngineArchitecture.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
 	//Engine pong ("Claire's Game Engine", { new Pong }, IRenderer::RendererType::SDL);
 	//Engine platformer("SDL 2D Platformer", { new Platformer2D },IRenderer::RendererType::SDL);
 	//Engine bowling("OpenGL 3D Bowling", { new Bowling }, IRenderer::RendererType::OPENGL);
-	Engine doom("OpenGL 3D Doom", { new Doom }, IRenderer::RendererType::OPENGL);
+	Engine doom("OpenGL 3D Not really Doom", { new Doom }, IRenderer::RendererType::OPENGL);
 
 	return 0;
 }

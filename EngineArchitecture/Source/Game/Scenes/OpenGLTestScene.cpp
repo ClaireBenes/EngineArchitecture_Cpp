73953a2335507257aa3bafc7b1aa0f49d423738d
Benes Claire <claire.benes@artfx.fr>
2025-09-29 10:46:26 +0200

    [chore] clean OpenGLTestScene

--- EngineArchitecture/Source/Game/Scenes/OpenGLTestScene.cpp
+++ EngineArchitecture/Source/Game/Scenes/OpenGLTestScene.cpp
@@ -14,40 +14,38 @@
 void OpenGLTestScene::Load()
 {
     mFloorMesh = AssetManager::LoadMesh("plane.obj", "cube");
-    mFloorMesh->SetShaderProgram(RendererGL::mSandShaderProgram);
+    mFloorMesh->SetShaderProgram(RendererGL::mSandShaderProgram); //RendererGL::mSimpleMeshShaderProgram
 
+    //Sand
     AssetManager::LoadTexture(*GetRenderer(), "Resources/Textures/sand.png", "sandTex");
     mFloorMesh->AddTexture(AssetManager::GetTexture("sandTex"));
 
+    //// ----- GRASS -----
     //AssetManager::LoadTexture(*GetRenderer(), "Resources/Textures/grass.png", "floor");
     //mFloorMesh->AddTexture(AssetManager::GetTexture("floor"));
+    // 
+    //mGrassMesh = AssetManager::LoadMesh("plant.obj", "grassBlade");
+    //mGrassMesh->SetShaderProgram(RendererGL::mGrassShaderProgram);
 
-    mGrassMesh = AssetManager::LoadMesh("plant.obj", "grassBlade");
-    mGrassMesh->SetShaderProgram(RendererGL::mGrassShaderProgram);
+    //AssetManager::LoadTexture(*GetRenderer(), "Resources/Textures/plant3.png", "plant");
+    //mGrassMesh->AddTexture(AssetManager::GetTexture("plant"));
+    //// ----- END GRASS -----
 
-    AssetManager::LoadTexture(*GetRenderer(), "Resources/Textures/plant3.png", "plant");
-    mGrassMesh->AddTexture(AssetManager::GetTexture("plant"));
-
-    //Sky
+    // ----- SKY -----
     Mesh* skySphere = AssetManager::LoadMesh("sphere.obj", "sphere");
     skySphere->SetShaderProgram(RendererGL::mSimpleMeshShaderProgram);
 
     AssetManager::LoadTexture(*GetRenderer(), "Resources/Textures/sky.png", "skyTex");
     skySphere->AddTexture(AssetManager::GetTexture("skyTex"));
-    
+    // ----- END SKY -----
+
+    // ----- Wave -----
     Mesh* waterMesh = AssetManager::LoadMesh("plane.obj", "plane");
     waterMesh->SetShaderProgram(RendererGL::mWaveShaderProgram);
 
     AssetManager::LoadTexture(*GetRenderer(), "Resources/Textures/voronoise.png", "waveTex");
     waterMesh->AddTexture(AssetManager::GetTexture("waveTex"));
-    //Sand
-    //Mesh* sandMesh = AssetManager::LoadMesh("plane.obj", "plane");
-    //sandMesh->SetShaderProgram(RendererGL::mSandShaderProgram);
-
-    //AssetManager::LoadTexture(*GetRenderer(), "Resources/Textures/sand.png", "sandTex");
-    //AssetManager::LoadTexture(*GetRenderer(), "Resources/Textures/sandDisp.jpg", "sandDisp");
-    //sandMesh->AddTexture(AssetManager::GetTexture("sandTex"));
-    //sandMesh->AddTexture(AssetManager::GetTexture("sandDisp"));
+    // ----- END WAVE -----
 }
 
 void OpenGLTestScene::Start()
@@ -55,6 +53,13 @@ void OpenGLTestScene::Start()
     SDL_SetHintWithPriority(SDL_HINT_MOUSE_RELATIVE_MODE_WARP, "1", SDL_HINT_OVERRIDE);
     SDL_SetRelativeMouseMode(SDL_TRUE);
 
+    Floor* floor = new Floor();
+    floor->SetTextureTiling(2, 2);
+    AddActor(floor);
+    floor->mTransform->mScale = Vector3(11, 0.0f, 10);
+    floor->mTransform->mPosition = { -0.6f + floor->mTransform->mScale.x, -2.5f, 11 };
+
+    // ----- SKY -----
     Actor* sky = new Actor();
     sky->mTransform->mPosition = { 0, -250, 10 };
     sky->mTransform->mScale = { 2000.0f, 2000.0f, 2000.0f };
@@ -62,30 +67,22 @@ void OpenGLTestScene::Start()
 
     MeshComponent* meshComponent = new MeshComponent(sky, AssetManager::GetMesh("sphere"));
     AddActor(sky);
+    // ----- END SKY -----
 
-    Floor* floor = new Floor();
-    floor->SetTextureTiling(2, 2);
-    AddActor(floor);
-    floor->mTransform->mScale = Vector3(11, 0.0f, 10);
-    floor->mTransform->mPosition = { -0.6f + floor->mTransform->mScale.x, -2.5f, 11 };
-
-    Floor* floor2 = new Floor();
-    floor2->SetTextureTiling(2, 2);
-    AddActor(floor2);
-    floor2->mTransform->mScale = Vector3(11, 0.0f, 10);
-    floor2->mTransform->mPosition = { -8, -2.5f, 20 };
-    floor2->mTransform->RotateYaw(-90);
-
+    // ----- Wave -----
     Actor* waves = new Actor();
     waves->mTransform->mScale = Vector3(10, 1.0f, 10);
     waves->mTransform->mPosition = Vector3(-0.6f + floor->mTransform->mScale.x, -2, 15);
     MeshComponent* wavesMeshComponent = new MeshComponent(waves, AssetManager::GetMesh("plane"));
     AddActor(waves);
     waves->mTransform->RotateYaw(20);
+    // ----- END WAVE -----
 
+    // ----- GRASS -----
     //Grass* grass = new Grass();
     //AddActor(grass);
     //grass->mTransform->mPosition = { 0, -2.5f, 2 };
+    // ----- END GRASS -----
 
     mCamera = new Camera();
     AddActor(mCamera);
